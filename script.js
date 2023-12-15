ad2284851aa615a15c79d6409f6691649cfeafec
Bernardo Euler <77172676+bernardoeuler@users.noreply.github.com>
2023-12-15 15:26:20 -0300

    Change layout and logic

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var btn = document.querySelector('button')
-var startNumber = document.getElementById('start')
-var endNumber = document.getElementById('end')
-var increment = document.getElementById('inc')
-var txtArea = document.getElementById('txtarea')
+const btn = document.querySelector('button')
+const startNumber = document.getElementById('start')
+const endNumber = document.getElementById('end')
+const increment = document.getElementById('inc')
+const txtArea = document.getElementById('txtarea')
+const separator = document.getElementById('sep')
 
 btn.addEventListener('click', () => {
-    
     const start = Number(startNumber.value)
     const end = Number(endNumber.value)
     const inc = Number(increment.value)
-    
+    const sep = separator.value
 
+    txtArea.value = ""
 
-    for (var i = start ; i <= end ; i = i + inc) {
-        console.log(i)
-        txtArea.innerText =+ i
+    for (let i = start ; i <= end ; i = i + inc) {
+        if (i === end ) return txtArea.value += i
+        txtArea.value += i + sep
     }
-    
 })
